23433cf8cc750a008616e7987d51377cb331415e
aswathy <aswathyvijayan1994@gmail.com>
2017-11-14 11:36:46 +0530

    included static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,10 +114,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'staticfiles'),
+)
+
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 ALLOWED_HOSTS = ['127.0.0.1','ErzaScarlet.pythonanywhere.com']
--- registrationmod/views.py
+++ registrationmod/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'home.html', context=None)
+
+class RegisterUser(TemplateView):
+    template_name = "registration.html"
